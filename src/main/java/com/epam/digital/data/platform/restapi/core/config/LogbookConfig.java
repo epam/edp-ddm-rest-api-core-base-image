d603ea5645042dfb326edc03c80dcef039fea893
Yevhen Zvarych <yevhen_zvarych@epam.com>
2021-09-27 12:20:14 +0000

    [EDP-DDM-0000] - Added custom logbook filter

---
+++ src/main/java/com/epam/digital/data/platform/restapi/core/config/LogbookConfig.java
@@ -0,0 +1,39 @@
+package com.epam.digital.data.platform.restapi.core.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.autoconfigure.LogbookProperties;
+import org.zalando.logbook.servlet.LogbookFilter;
+
+import javax.servlet.DispatcherType;
+
+@Configuration
+public class LogbookConfig {
+
+  private final LogbookProperties logbookProperties;
+
+  public LogbookConfig(LogbookProperties logbookProperties) {
+    this.logbookProperties = logbookProperties;
+  }
+
+  @Bean
+  @ConditionalOnProperty(
+      name = {"logbook.filter.enabled"},
+      havingValue = "true",
+      matchIfMissing = true)
+  public FilterRegistrationBean<LogbookFilter> logbookFilter(Logbook logbook) {
+    var filter =
+        new LogbookFilter(logbook)
+            .withFormRequestMode(logbookProperties.getFilter().getFormRequestMode());
+    var registration = new FilterRegistrationBean<>(filter);
+    registration.setName("logbookFilter");
+    registration.setDispatcherTypes(
+            DispatcherType.REQUEST, DispatcherType.ASYNC);
+    registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return registration;
+  }
+}
